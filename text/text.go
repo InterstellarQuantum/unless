7236275d39f12cee4dab3f536f0ab6ca3372cfee
duzy555666 <965876239@qq.com>
2022-01-27 11:25:54 +0800

    操作文本

---
+++ text/text.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+)
+
+//操作文本,主要目的是写入,把内容换行追加在文本里面
+
+func Preserve(path string) (func(text string) error, error) {
+	_, err := os.Stat(path)
+	if err != nil {
+		_, err := os.Create(path)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return func(text string) error {
+		openFile, e := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, 0666)
+		if e != nil {
+			return e
+		}
+		_, err := openFile.WriteString(text + "\n")
+		if err != nil {
+			return err
+		}
+		return nil
+	}, nil
+}
